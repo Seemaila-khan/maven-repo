9c7401f1e4788f248c87d9fe6b15a583ed541524
Seemaila-khan <Seemailakhan123@gmail.com>
2020-12-21 15:10:44 +0530

    added mul method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,10 +8,14 @@ public class Calc {
 	public static int subs(int n1, int n2) {
 		return n1 - n2;
 	}
+	public static int mul(int n1, int n2) {
+		return n1 * n2;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("sum =" + add(10, 20));
 		System.out.println("sub =" + subs(10, 20));
+		System.out.println("prod =" + mul(10, 20));
 	}
 
 }
